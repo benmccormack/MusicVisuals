ec9e97425dc7b116fb0cfcb565ad26abe0d83b4e
benmccormack <c19459424@mytudublin.ie>
2021-03-31 15:41:32 +0100

    setup

---
+++ java/src/c19459424/MyVisual.java
@@ -0,0 +1,53 @@
+package c19459424;
+
+import ie.tudublin.*;
+
+public class MyVisual extends Visual {
+    waveform wf;
+
+    public void settings()
+    {
+        size(1024,1024, P3D);
+    }
+
+    public void setup()
+    {
+        startMinim();
+                
+        // Call loadAudio to load an audio file to process 
+        loadAudio("heroplanet.mp3");   
+        
+        // Call this instead to read audio from the microphone
+        //startListening(); 
+        wf = new waveform(this);
+    }
+
+    public void keyPressed()
+    {
+        if (key == ' ')
+        {
+            getAudioPlayer().cue(0);
+            getAudioPlayer().play();
+        }
+    }
+
+    public void draw()
+    {
+        background(0);
+        try
+        {
+            // Call this if you want to use FFT data
+            calculateFFT(); 
+        }
+        catch(VisualException e)
+        {
+            e.printStackTrace();
+        }
+        // Call this is you want to use frequency bands
+        calculateFrequencyBands(); 
+
+        // Call this is you want to get the average amplitude
+        calculateAverageAmplitude();     
+        wf.render();
+    }
+}
